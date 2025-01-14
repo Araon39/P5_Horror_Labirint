0fe8b151260d7ae9e5d28129a0d405c578e6da8d
Araon39 <115640752+Araon39@users.noreply.github.com>
2025-01-14 11:49:02 +0200

    AddControler
    Создали контролер скрипт для управления мышью а также настройка чувствительности в инпут менеджере

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] int speed = 20;
+    [SerializeField] int rotationSpeed = 90;
+
+    void Update()
+    {
+        // Получаем значения осей от клавиатуры
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Получаем значение оси от мыши
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        // Перемещаем объект вперед/назад и влево/вправо
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vertical);
+
+        // Вращаем объект вокруг оси Y по горизонтальной оси клавиатуры
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * horizontal);
+
+        // Вращаем объект вокруг оси Y по горизонтальной оси мыши
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * mouseHorizontal);
+    }
+}
+
+public class PlayerController2 : MonoBehaviour
+{
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float rotationSpeed = 90f;
+
+    void Update()
+    {
+        // Получаем значения осей от клавиатуры
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Получаем значение оси от мыши
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        // Перемещаем объект вперед/назад
+        Vector3 movement = transform.forward * speed * Time.deltaTime * vertical;
+        transform.Translate(movement, Space.World);
+
+        // Вращаем объект вокруг оси Y по горизонтальной оси клавиатуры
+        float rotation = rotationSpeed * Time.deltaTime * horizontal;
+        transform.Rotate(Vector3.up, rotation);
+
+        // Вращаем объект вокруг оси Y по горизонтальной оси мыши
+        float mouseRotation = rotationSpeed * Time.deltaTime * mouseHorizontal;
+        transform.Rotate(Vector3.up, mouseRotation);
+    }
+}
